6eb80510cc1ca97721c571f790c019568e10dcb5
anilalis <anildervisogluib@gmail.com>
2023-02-23 19:24:00 +0300

    some extra functions for server and client, mostly from libft

---
+++ minitalk_utils.c
@@ -0,0 +1,51 @@
+#include "minitalk.h"
+
+void ft_itoa(int num)
+{
+    char rt;
+    if (num >= 10)
+        ft_itoa(num / 10);
+    rt = num % 10 + '0';
+    write(1, &rt, 1);
+}
+
+int ft_atoi(char *str)
+{
+    int num;
+
+    num = 0;
+    while (*str)
+    {
+        if (*str >= '0' && *str <= '9')
+            num = num * 10 + *str - '0';
+        else
+            exit(1); // + error message
+        str++;
+    }
+    return (num);
+}
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < len)
+		*(unsigned char *)(b + i++) = (unsigned char)c;
+	return (b);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, 0, n);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
